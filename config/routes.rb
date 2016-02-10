c18269efef36fe03533b82d1bff9b9a623c1e9d9
Gabriel Carpenedo <gabriel.carpenedo@gmail.com>
2016-02-10 19:59:14 -0200

    Creates works page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root 'pages#home'
   post 'contato', to: 'pages#contact', as: :contact
 
+  get 'trabalhos', to: 'pages#works', as: :works
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
