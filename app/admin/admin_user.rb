dc38fc010d8b39b81058cd092f8e83fa197620e9
Patrick Müller <patrick.e.muller@gmail.com>
2016-03-08 17:17:26 -0300

    Adds Avatar & Bio fields to AdminUser Model

--- app/admin/admin_user.rb
+++ app/admin/admin_user.rb
@@ -1,9 +1,23 @@
 ActiveAdmin.register AdminUser do
-  permit_params :name, :email, :password, :password_confirmation
+  permit_params :name, :email, :password, :password_confirmation, :avatar,
+                :avatar_cache, :bio
+
+  controller do
+    def update
+      if params[:admin_user][:password].blank?
+        params[:admin_user].delete("password")
+        params[:admin_user].delete("password_confirmation")
+      end
+      super
+    end
+  end
 
   index do
     selectable_column
     id_column
+    column :avatar do |admin|
+      image_tag admin.avatar.url, size: '100x100' if admin.avatar.present?
+    end
     column :name
     column :email
     column :current_sign_in_at
@@ -18,10 +32,24 @@ ActiveAdmin.register AdminUser do
   filter :sign_in_count
   filter :created_at
 
+  show do
+    attributes_table do
+      row :avatar do |admin|
+        image_tag admin.avatar.url, size: '200'
+      end
+      row :name
+      row :email
+      row :bio
+    end
+  end
+
   form do |f|
     f.inputs "Admin Details" do
+      f.input :avatar
+      f.input :avatar_cache, as: :hidden
       f.input :name
       f.input :email
+      f.input :bio
       f.input :password
       f.input :password_confirmation
     end
