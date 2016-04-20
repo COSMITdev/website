a720d149077c95d9485c04c5169203f2ee541565
Patrick Müller <patrick.e.muller@gmail.com>
2016-04-20 17:50:25 -0300

    Fixes problems with location on homepage

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,16 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
-  # scope '(:locale)', locale: /pt-BR|en/ do
-    localized do
-      root 'pages#home'
-      get 'work',      to: 'pages#works',    as: :work
-      get 'services',  to: 'pages#services', as: :service
-      get '404',       to: 'pages#404',      as: :not_found
-      post 'contact',  to: 'pages#contact',  as: :contact
-    end
+  scope '(:locale)', locale: /pt-BR|en/ do
+    root 'pages#home'
+  end
+
+  localized do
+    get 'work',      to: 'pages#works',    as: :work
+    get 'services',  to: 'pages#services', as: :service
+    get '404',       to: 'pages#404',      as: :not_found
+    post 'contact',  to: 'pages#contact',  as: :contact
+  end
   # end
 
   resources :posts, controller: 'blog', path: 'blog', only: [:index, :show]
