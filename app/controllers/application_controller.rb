951e4ea820a45c687b752737610afbf093984fd4
Patrick Müller <patrick.e.muller@gmail.com>
2016-04-21 19:12:43 -0300

    Adds console logs for IP requests on application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,13 +15,21 @@ class ApplicationController < ActionController::Base
   end
 
   def set_locale
+    location == 'EN' # default
+
     begin
       location  = request.location.country_code
     rescue Errno::EHOSTUNREACH, Errno::ETIMEDOUT, Errno::ENETUNREACH, Geocoder::NetworkError
       # primary service unreachable, try secondary...
       page     = Net::HTTP.get(URI('https://geoip-db.com/json/geoip.php'))
       location = JSON.parse(page)['country_code']
+      printf JSON.parse(page)['country_name']
+      printf JSON.parse(page)['country_code']
+      printf JSON.parse(page)['IPv4']
+      printf JSON.parse(page)['city']
     end
+
+    printf location
 
     I18n.locale = params[:locale] || (location == 'BR' ? :'pt-BR' : :en)
   end
