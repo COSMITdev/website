cdc2d7ed72ec8d7c6336449e37630936f7e487c5
Patrick Müller <patrick.e.muller@gmail.com>
2016-04-21 19:58:34 -0300

    Adds workaround when Geocoder first IP lookup is offline

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,18 +15,12 @@ class ApplicationController < ActionController::Base
   end
 
   def set_locale
-    location == 'EN' # default
-
     begin
-      location  = request.location.country_code
+      location = request.location.country_code
     rescue Errno::EHOSTUNREACH, Errno::ETIMEDOUT, Errno::ENETUNREACH, Geocoder::NetworkError
       # primary service unreachable, try secondary...
-      page     = Net::HTTP.get(URI('https://geoip-db.com/json/geoip.php'))
-      location = JSON.parse(page)['country_code']
-      printf JSON.parse(page)['country_name']
-      printf JSON.parse(page)['country_code']
-      printf JSON.parse(page)['IPv4']
-      printf JSON.parse(page)['city']
+      Geocoder.configure(ip_lookup: :ipinfo_io)
+      location = request.location.country_code
     end
 
     printf location
