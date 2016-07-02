d10f24577566ef81edd9bc6c26c5c9d207a22c1c
Patrick Müller <patrick.e.muller@gmail.com>
2016-07-02 18:20:46 -0300

    Fixes problems with routes on heroku

--- config/routes.rb
+++ config/routes.rb
@@ -3,23 +3,19 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
 
   scope '(:locale)', locale: /pt-BR|en/ do
-    constraints subdomain: '' do
-      root to: 'pages#home', as: :root
-    end
+    match '/', to: 'pages#home', constraints: { subdomain: '' }, as: :root, via: 'get'
+    match '/', to: 'pages#mvp', constraints: { subdomain: /.+/ }, as: :mvp, via: 'get'
 
-    constraints subdomain: 'mvp' do
-      root to: 'pages#mvp', as: :mvp
+    localized do
+      get 'work',      to: 'pages#works',    as: :work
+      get 'services',  to: 'pages#services', as: :service
+      get '404',       to: 'pages#404',      as: :not_found
+      post 'contact',  to: 'pages#contact',  as: :contact
+
+      resources :posts, controller: 'blog', path: 'blog', only: [:index, :show]
     end
   end
 
-  localized do
-    get 'work',      to: 'pages#works',    as: :work
-    get 'services',  to: 'pages#services', as: :service
-    get '404',       to: 'pages#404',      as: :not_found
-    post 'contact',  to: 'pages#contact',  as: :contact
-
-    resources :posts, controller: 'blog', path: 'blog', only: [:index, :show]
-  end
   # Keep this always at the end of file to grab 404 problems
   # get '*path', to: 'pages#404', via: :all
 end
