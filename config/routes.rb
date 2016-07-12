a9e18e05a97a25abbaac8e5ced4471857b7ee0ea
Gabriel Carpenedo <cbgabe@gmail.com>
2016-07-11 23:08:09 -0400

    Removes subdomains

--- config/routes.rb
+++ config/routes.rb
@@ -3,40 +3,20 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
 
   scope '(:locale)', locale: /pt-BR|en/ do
-    constraints subdomain: '' do
-      # For some unknown reason this routes don't work
-      # when declared as 'root to'
-      match '/', to: 'pages#home', as: :root, via: 'get'
-      resources :posts, controller: 'blog', path: 'blog', only: [:index, :show]
+    root 'pages#home'
+  end
 
-      localized do
-        get 'work',      to: 'pages#works',    as: :work
-        get 'services',  to: 'pages#services', as: :service
-        get '404',       to: 'pages#404',      as: :not_found
-      end
-    end
+  localized do
+    get 'work',      to: 'pages#works',    as: :work
+    get 'services',  to: 'pages#services', as: :service
+    get '404',       to: 'pages#404',      as: :not_found
+    post 'contact',  to: 'pages#contact',  as: :contact
+    get 'mvp',       to: 'mvp#index',      as: :mvp
 
-    constraints subdomain: 'mvp' do
-      # For some unknown reason this routes don't work
-      # when declared as 'root to'
-      match '/', to: 'mvp#index', as: :mvp, via: 'get'
-    end
-
-    constraints subdomain: 'course' do
-      # For some unknown reason this routes don't work
-      # when declared as 'root to'
-      match '/', to: 'subscriptions#index', as: :course, via: 'get'
-      get 'unsubscribe', to: 'subscriptions#unsubscribe', as: :unsubscribe
-      get 'next', to: 'subscriptions#next', as: :next_newsletter
-    end
-
-    # This routes don't need to be translated
-    # and do not try to put them inside constraints
-    # won't gonna work...
+    resources :posts, controller: 'blog', path: 'blog', only: [:index, :show]
     resource :contact, only: :create
     resource :subscription, only: [:create, :destroy]
   end
 
-  # Keep this always at the end of file to grab 404 problems
   get '*path', to: 'pages#404', via: :all
 end
