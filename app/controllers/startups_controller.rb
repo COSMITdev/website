0bae7bdab603eab22dbfd0507ef45e18b386f24e
Gabriel Carpenedo <cbgabe@gmail.com>
2016-08-06 16:44:43 -0400

    Improves startups controller

--- app/controllers/startups_controller.rb
+++ app/controllers/startups_controller.rb
@@ -1,20 +1,15 @@
 class StartupsController < ApplicationController
-  before_action :set_contact
+  before_action :set_contact, on: [:index, :strategycall, :mvpconcierge, :mvpslim]
 
-  def index
-  end
+  def index;end
 
-  def validation
-  end
+  def validation;end
 
-  def strategycall
-  end
+  def strategycall;end
 
-  def mvpconcierge
-  end
+  def mvpconcierge;end
 
-  def mvpslim
-  end
+  def mvpslim;end
 
   private
 
