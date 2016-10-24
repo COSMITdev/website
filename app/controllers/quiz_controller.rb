dea021307c5d98f871c4c2f4579bbe911ab39791
Gabriel Carpenedo <cbgabe@gmail.com>
2016-10-24 17:54:37 -0200

    Improves quiz link

--- app/controllers/quiz_controller.rb
+++ app/controllers/quiz_controller.rb
@@ -1,10 +1,4 @@
 class QuizController < ApplicationController
   def quiz1
   end
-
-  def quiz2
-  end
-
-  def quiz3
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,21 +4,12 @@ Rails.application.routes.draw do
 
   root 'pages#home'
 
-  get 'quiz1',                        to: 'quiz#quiz1',            as: :quiz1
-  get 'quiz2',                        to: 'quiz#quiz2',            as: :quiz2
-  get 'quiz3',                        to: 'quiz#quiz3',            as: :quiz3
-  get 'servicos',                     to: 'pages#services',        as: :service
-  get 'trabalhos',                    to: 'pages#works',           as: :work
-  # get 'course',                     to: 'course#index',          as: :course
-  # get 'mvpslim',                    to: 'mvp#index',             as: :mvp
-  # get 'startups',                   to: 'startups#index',        as: :startups
-  # get 'startups/validation-course', to: 'startups#validation',   as: :validation
-  # get 'startups/strategy-call',     to: 'startups#strategycall', as: :strategy_call
-  # get 'startups/mvp-concierge',     to: 'startups#mvpconcierge', as: :mvp_concierge
-  # get 'startups/mvp-slim',          to: 'startups#mvpslim',      as: :mvp_slim
-  post 'contato',                     to: 'contacts#create',       as: :contact
-  post 'inscricao',                   to: 'subscribers#create',    as: :subscribe
-  post 'newsletter',                  to: 'newsletter#create',     as: :newsletter
+  get 'descubra-se-sua-ideia-esta-em-risco',  to: 'quiz#quiz1',            as: :quiz1
+  get 'servicos',                             to: 'pages#services',        as: :service
+  get 'trabalhos',                            to: 'pages#works',           as: :work
+  post 'contato',                             to: 'contacts#create',       as: :contact
+  post 'inscricao',                           to: 'subscribers#create',    as: :subscribe
+  post 'newsletter',                          to: 'newsletter#create',     as: :newsletter
 
   resources :posts, controller: 'blog', path: 'blog', only: [:index, :show]
 
