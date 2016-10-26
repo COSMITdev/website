bae1d6879d380441e0d2bf11e189a64ec7f95622
Gabriel Carpenedo <cbgabe@gmail.com>
2016-10-26 16:44:43 -0200

    Creates protocamp page

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,13 @@ Rails.application.routes.draw do
 
   root 'pages#home'
 
-  get 'descubra-se-sua-ideia-esta-em-risco',  to: 'quiz#quiz1',            as: :quiz1
-  get 'servicos',                             to: 'pages#services',        as: :service
-  get 'trabalhos',                            to: 'pages#works',           as: :work
-  post 'contato',                             to: 'contacts#create',       as: :contact
-  post 'inscricao',                           to: 'subscribers#create',    as: :subscribe
-  post 'newsletter',                          to: 'newsletter#create',     as: :newsletter
+  get '/quiz/descubra-se-sua-ideia-esta-em-risco',  to: 'quiz#quiz1',            as: :quiz1
+  get 'protocamp',                                  to: 'protocamp#index',       as: :protocamp
+  get 'servicos',                                   to: 'pages#services',        as: :service
+  get 'trabalhos',                                  to: 'pages#works',           as: :work
+  post 'contato',                                   to: 'contacts#create',       as: :contact
+  post 'inscricao',                                 to: 'subscribers#create',    as: :subscribe
+  post 'newsletter',                                to: 'newsletter#create',     as: :newsletter
 
   resources :posts, controller: 'blog', path: 'blog', only: [:index, :show]
 
