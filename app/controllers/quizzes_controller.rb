5e0aa9e38e9d71c4ab8e0d5cebf808c5c55fb6ca
Patrick Müller <patrick.e.muller@gmail.com>
2016-11-03 20:02:45 -0200

    Adds User from Quiz to ActiveCampaign after Quiz done

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -10,7 +10,7 @@ class QuizzesController < ApplicationController
     @quiz = Quiz.new(permitted_params)
 
     if @quiz.valid?
-      # send to active campaign list
+      subscribe_to_active_campaign(@quiz)
       render :result
     else
       render :create
@@ -18,6 +18,23 @@ class QuizzesController < ApplicationController
   end
 
   private
+
+  def subscribe_to_active_campaign(quiz)
+    name = quiz.name
+    email = quiz.email
+
+    if Rails.env.production?
+      query = JSON.parse(Campaign.contact_sync(email: email,
+        first_name: name, 'p[2]' => 2, 'status[2]' => 1,
+        tags: 'quiz,sw'))
+    else
+      query = JSON.parse(Campaign.contact_sync(email: email,
+        first_name: name, 'p[1]' => 1, 'status[1]' => 1,
+        tags: 'quiz,sw'))
+    end
+
+    query['result_code'] == 1 # return true or false, code 1 is for successful requests
+  end
 
   def permitted_params
     params.require(:quiz).permit(:q1, :q2, :q3, :q4, :q5, :name, :email)
