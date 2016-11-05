e202066004c71e24f95d6f301c957b788a6547b5
Patrick Müller <patrick.e.muller@gmail.com>
2016-11-05 14:25:33 -0200

    Improves URLs for Quiz

--- config/routes.rb
+++ config/routes.rb
@@ -4,17 +4,16 @@ Rails.application.routes.draw do
 
   root 'pages#home'
 
-  get 'protocamp',                                  to: 'protocamp#index',       as: :protocamp
-  get 'servicos',                                   to: 'pages#services',        as: :service
-  get 'trabalhos',                                  to: 'pages#works',           as: :work
-  post 'contato',                                   to: 'contacts#create',       as: :contact
-  post 'inscricao',                                 to: 'subscribers#create',    as: :subscribe
-  post 'newsletter',                                to: 'newsletter#create',     as: :newsletter
+  get 'servicos',    to: 'pages#services',     as: :service
+  get 'protocamp',   to: 'protocamp#index',    as: :protocamp
+  get 'trabalhos',   to: 'pages#works',        as: :work
+  post 'contato',    to: 'contacts#create',    as: :contact
+  post 'inscricao',  to: 'subscribers#create', as: :subscribe
+  post 'newsletter', to: 'newsletter#create',  as: :newsletter
 
   resources :posts, controller: 'blog', path: 'blog', only: [:index, :show]
-
-  get 'descubra-se-sua-ideia-esta-em-risco', to: 'quizzes#new', as: :new_quiz
-  resources :quizzes, only: [:index, :create]
+  resources :quizzes, path: 'descubra-se-sua-ideia-esta-em-risco', only: [:index, :create]
+  get 'descubra-se-sua-ideia-esta-em-risco/quiz', to: 'quizzes#new', as: :new_quiz
 
   get '*path', to: 'pages#404', via: :all
 end
