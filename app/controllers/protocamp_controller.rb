390a9054ec528967b1d0b417cb95eda14518579a
Patrick Müller <patrick.e.muller@gmail.com>
2016-11-06 16:33:08 -0200

    Organize and move "subscribers" to ProtoCamp controller
    This "subscribers" in fact are the same person from Protocamp, but the action here is to download the PROTOCAMP modal, not subscribe to another thing, in this case makes more sense them be inside provocam controller & views

--- app/controllers/protocamp_controller.rb
+++ app/controllers/protocamp_controller.rb
@@ -7,27 +7,40 @@ class ProtocampController < ApplicationController
     @protocamp = Protocamp.new(permitted_params)
 
     if @protocamp.valid?
-      subscribe_to_active_campaign(@protocamp)
-      render :success
-    else
-      render :error
+      subscribe_to_active_campaign(@protocamp, 'protocamp,sl')
     end
+
+    render :create
+  end
+
+  def canvas
+    @canvas = Protocamp.new(permitted_params)
+    # Used for facebook/twitter share buttons
+    @post = Post.friendly.find request.referer.split('/').last
+
+    if @canvas.valid?
+      subscribe_to_active_campaign(@canvas, 'blog,canvas,sw')
+      @canvas = Protocamp.new # empty fields on form
+      @message = I18n.t('canvas.success')
+    else
+      @message = I18n.t('canvas.failure')
+    end
+
+    render :canvas
   end
 
   private
 
-  def subscribe_to_active_campaign(protocamp)
-    name = protocamp.name
-    email = protocamp.email
+  def subscribe_to_active_campaign(user, tags)
+    name = user.name
+    email = user.email
 
     if Rails.env.production?
       query = JSON.parse(Campaign.contact_sync(email: email,
-        first_name: name, 'p[2]' => 2, 'status[2]' => 1,
-        tags: "protocamp,sl"))
+        first_name: name, 'p[2]' => 2, 'status[2]' => 1, tags: tags))
     else
       query = JSON.parse(Campaign.contact_sync(email: email,
-        first_name: name, 'p[1]' => 1, 'status[1]' => 1,
-        tags: "protocamp,sl"))
+        first_name: name, 'p[1]' => 1, 'status[1]' => 1, tags: tags))
     end
 
     query['result_code'] == 1 # return true or false, code 1 is for successful requests
--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,13 @@ Rails.application.routes.draw do
 
   root 'pages#home'
 
-  get 'servicos',    to: 'pages#services',     as: :service
-  get 'trabalhos',   to: 'pages#works',        as: :work
-  get 'protocamp',   to: 'protocamp#index',    as: :protocamp
-  post 'contato',    to: 'contacts#create',    as: :contact
-  post 'inscricao',  to: 'subscribers#create', as: :subscribe
-  post 'newsletter', to: 'newsletter#create',  as: :newsletter
+  get 'servicos',    to: 'pages#services',    as: :service
+  get 'trabalhos',   to: 'pages#works',       as: :work
+  get 'protocamp',   to: 'protocamp#index',   as: :protocamp
+  post 'contato',    to: 'contacts#create',   as: :contact
+  post 'newsletter', to: 'newsletter#create', as: :newsletter
   post 'protocamp',  to: 'protocamp#create'
+  post 'inscricao',  to: 'protocamp#canvas',  as: :canvas
 
   resources :posts, controller: 'blog', path: 'blog', only: [:index, :show]
   resources :quizzes, path: 'descubra-se-sua-ideia-esta-em-risco', only: [:index, :create]
